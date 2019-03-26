abec0a1fb3b09361138cdef3c4dc0352ee794777
Maria Podryadchikova <mpodr2015@gmail.com>
2019-03-26 18:44:49 +0300

    xrenner practical

--- 2018-komp-ling/practicals/udpipe_practical/xrenner_practice.py
+++ 2018-komp-ling/practicals/udpipe_practical/xrenner_practice.py
@@ -1,14 +1,15 @@
 import xrenner
 
-x = xrenner.Xrenner(model = "/home/laidhimonthegreen/udpipe/src/UD_Russian-SynTagRus/xrenner/xrenner/models/rus")
+x = xrenner.Xrenner(model = "/home/laidhimonthegreen/udpipe/src/UD_Russian-SynTagRus/xrenner/xrenner/models/eng")
 
-"""
+
 #Часть один, про Новую Зеландию
 res = x.analyze('example_in.conll10', "html")
 #записываем результаты в html
 open('coref.html', 'w+').write(res)
-"""
+
+rusx = xrenner.Xrenner(model = "/home/laidhimonthegreen/udpipe/src/UD_Russian-SynTagRus/xrenner/xrenner/models/rus")
 
 #Часть два, про Пушкина.
-res = x.analyze('pushkin.conllu', "html")
+res = rusx.analyze('pushkin.conllu', "html")
 open('coref_pushkin.html', 'w+').write(res)
